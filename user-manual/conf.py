6cf222f3dc1a9fb80617b01b15cfae7f304ce2db
Davide Principi <davide.principi@nethesis.it>
2018-06-28 12:24:39 +0200

    Parameters for Languages and Versions

--- user-manual/conf.py
+++ user-manual/conf.py
@@ -17,14 +17,7 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-try:
-    import sphinx_bootstrap_theme
-except ImportError:
-    pass
+import sphinx_bootstrap_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -90,7 +83,6 @@ todo_include_todos = False
 #
 html_theme = 'bootstrap'
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-
 html_theme_options = {
         'navbar_title': 'NethCTI',
         'navbar_pagenav': True,
@@ -189,3 +181,19 @@ texinfo_documents = [
 if os.path.exists('rst_prolog'):
     with open('rst_prolog') as fid:
         rst_prolog = fid.read()
+
+#
+# Define context default values for HTML templates
+#
+context = {
+    'alt_languages': 'it,en,es',
+    'alt_versions': 'v3,v2,dev',
+    'current_version': 'def',
+    'user_analytics_code': 'UA-37499928-5',
+}
+
+if 'html_context' in globals():
+    for key in context:
+        html_context.setdefault(k, context[k])
+else:
+    html_context = context
